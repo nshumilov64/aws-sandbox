84dc6268ca347261ee7c68b3e74a4d314b08cc5a
nshumilov64 <nshumilov64@gmail.com>
2024-08-09 12:36:28 +0400

    Update syndicate CLI notes

--- task03/cli.ps1
+++ task03/cli.ps1
@@ -3,3 +3,14 @@ syndicate generate meta api_gateway `
     --deploy_stage api `
     --minimum_compression_size 0
 
+syndicate generate meta api_gateway_resource `
+    --api_name task3_api `
+    --path /hello `
+    --enable_cors false
+
+syndicate generate meta api_gateway_resource_method `
+    --api_name task3_api `
+    --path /hello `
+    --method GET `
+    --integration_type lambda `
+    --lambda_name hello_world
