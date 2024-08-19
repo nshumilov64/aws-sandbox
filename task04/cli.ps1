2c7b9e352deeddea06f86dc482017a057f9c226d
nshumilov64 <nshumilov64@gmail.com>
2024-08-19 08:21:39 +0400

    Generate lambda_topic

--- task04/cli.ps1
+++ task04/cli.ps1
@@ -2,3 +2,5 @@ syndicate generate project --name task04
 syndicate generate config
 syndicate generate lambda --name sqs_handler --runtime java
 syndicate generate meta sqs_queue --resource_name async_queue
+syndicate generate lambda --name sns_handler --runtime java
+syndicate generate meta sns_topic --resource_name lambda_topic --region eu-central-1
