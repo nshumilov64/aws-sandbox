ca1b1fb76bd88d9b138cd7e8eee5de066e31c01a
nshumilov64 <nshumilov64@gmail.com>
2024-08-19 10:23:09 +0400

    Generate the Events table

--- task05/cli.ps1
+++ task05/cli.ps1
@@ -1,22 +1,24 @@
 syndicate generate project --name task05
 syndicate generate lambda --name api_handler --runtime java
-
 syndicate generate meta api_gateway `
     --resource_name task5_api `
     --deploy_stage api `
     --minimum_compression_size 0
-
 syndicate generate meta api_gateway_resource `
     --api_name task5_api `
     --path /events `
     --enable_cors false
-
 syndicate generate meta api_gateway_resource_method `
     --api_name task5_api `
     --path /events `
     --method POST `
     --integration_type lambda `
     --lambda_name api_handler
+syndicate generate meta dynamodb `
+    --resource_name Events `
+    --hash_key_name id `
+    --hash_key_type S
+
 
 
 
