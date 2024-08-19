23a8b813e41a5be87e85feed1147b8082eb1bf17
nshumilov64 <nshumilov64@gmail.com>
2024-08-19 07:16:54 +0400

    Generate async_queue

--- task04/cli.ps1
+++ task04/cli.ps1
@@ -1,3 +1,4 @@
 syndicate generate project --name task04
 syndicate generate config
 syndicate generate lambda --name sqs_handler --runtime java
+syndicate generate meta sqs_queue --resource_name async_queue
