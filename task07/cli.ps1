85ffb630f39a9d3dd225ddb06deaf3f41f5d621a
nshumilov64 <nshumilov64@gmail.com>
2024-10-02 15:21:12 +0400

    Add requisite resources and dependencies

--- task07/cli.ps1
+++ task07/cli.ps1
@@ -1,3 +1,10 @@
 syndicate generate project task06
 cd task07
 syndicate generate config
+syndicate generate lambda --name uuid_generator --runtime java
+syndicate generate meta s3_bucket --resource_name uuid-storage --location eu-central-1
+syndicate generate meta cloudwatch_event_rule `
+    --resource_name uuid_trigger `
+    --rule_type schedule `
+    --expression "rate(1 minute)" `
+    --region eu-central-1
