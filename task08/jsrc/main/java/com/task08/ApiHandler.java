232eb5baf0ada5c8920fe7b8985a55b0f735b773
nshumilov64 <nshumilov64@gmail.com>
2024-10-03 06:28:49 +0400

    Properly attach the layer + add url config + add assembly zip to .gitignore

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -4,14 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import org.example.OpenMeteoClient;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-@LambdaHandler(lambdaName = "api_handler", roleName = "api_handler-role")
+@LambdaHandler(lambdaName = "api_handler", roleName = "api_handler-role", layers = {"sdk_layer"})
 @LambdaLayer(layerName = "sdk_layer", libraries = {"layer/open-meteo-client-0.1.jar"})
+@LambdaUrlConfig
 public class ApiHandler implements RequestHandler<Object, String> {
     public String handleRequest(Object request, Context context) {
         try {
