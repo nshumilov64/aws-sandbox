d2d48139579a0251c7e73e713a4e6426cbe271c7
nshumilov64 <nshumilov64@gmail.com>
2024-10-04 11:32:26 +0400

    Add api_gateway resources and methods

--- task10/cli.ps1
+++ task10/cli.ps1
@@ -2,11 +2,12 @@ syndicate generate project --name task10
 cd task10
 syndicate generate config
 syndicate generate lambda --name api_handler --runtime java
-#Cognito
+# Cognito
 syndicate generate meta cognito_user_pool --resource_name simple-booking-userpool
 # API Gateway
 syndicate generate meta api_gateway --resource_name task10_api --deploy_stage api
 syndicate generate meta api_gateway_authorizer --api_name task10_api --name authorizer --type COGNITO_USER_POOLS --provider_name simple-booking-userpool
+# Gateway resources and methods
 syndicate generate meta api_gateway_resource --api_name task10_api --path /signin --enable_cors true
 syndicate generate meta api_gateway_resource_method --api_name task10_api --path /signin --method POST --integration_type lambda --lambda_name api_handler
 syndicate generate meta api_gateway_resource --api_name task10_api --path /signup --enable_cors true
@@ -14,8 +15,8 @@ syndicate generate meta api_gateway_resource_method --api_name task10_api --path
 syndicate generate meta api_gateway_resource --api_name task10_api --path /tables --enable_cors true
 syndicate generate meta api_gateway_resource_method --api_name task10_api --path /tables --method GET --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
 syndicate generate meta api_gateway_resource_method --api_name task10_api --path /tables --method POST --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
-syndicate generate meta api_gateway_resource --api_name task10_api --path /tables/{tableId} --enable_cors true
-syndicate generate meta api_gateway_resource_method --api_name task10_api --path /tables/{tableId} --method GET --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
+syndicate generate meta api_gateway_resource --api_name task10_api --path "/tables/{tableId}" --enable_cors true
+syndicate generate meta api_gateway_resource_method --api_name task10_api --path "/tables/{tableId}" --method GET --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
 syndicate generate meta api_gateway_resource --api_name task10_api --path /reservations --enable_cors true
 syndicate generate meta api_gateway_resource_method --api_name task10_api --path /reservations --method GET --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
 syndicate generate meta api_gateway_resource_method --api_name task10_api --path /reservations --method POST --integration_type lambda --lambda_name api_handler --authorization_type CUSTOM --authorizer_name authorizer
