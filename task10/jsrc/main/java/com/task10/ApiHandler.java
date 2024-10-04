973356009f259eb5d70519732a9879b866075a57
nshumilov64 <nshumilov64@gmail.com>
2024-10-04 12:09:49 +0400

    Adjust dependencies + add requisite lambda config annotations

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -2,18 +2,31 @@ package com.task10;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 
-import java.util.HashMap;
-import java.util.Map;
+import static com.syndicate.deployment.model.environment.ValueTransformer.USER_POOL_NAME_TO_CLIENT_ID;
+import static com.syndicate.deployment.model.environment.ValueTransformer.USER_POOL_NAME_TO_USER_POOL_ID;
 
 @LambdaHandler(lambdaName = "api_handler", roleName = "api_handler-role")
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
-    public Map<String, Object> handleRequest(Object request, Context context) {
-        System.out.println("Hello from lambda");
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-        resultMap.put("statusCode", 200);
-        resultMap.put("body", "Hello from Lambda");
-        return resultMap;
+@DependsOn(name = "${booking_user_pool}", resourceType = ResourceType.COGNITO_USER_POOL)
+@DependsOn(name = "${tables_table}", resourceType = ResourceType.DYNAMODB_TABLE)
+@DependsOn(name = "${reservations_table}", resourceType = ResourceType.DYNAMODB_TABLE)
+@EnvironmentVariables(value = {
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "user_pool_id", value = "${booking_user_pool}",
+                valueTransformer = USER_POOL_NAME_TO_USER_POOL_ID),
+        @EnvironmentVariable(key = "client_id", value = "${booking_user_pool}",
+                valueTransformer = USER_POOL_NAME_TO_CLIENT_ID)
+})
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        return new APIGatewayProxyResponseEvent().withStatusCode(200);
     }
 }
